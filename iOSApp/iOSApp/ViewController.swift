0e8362ddc3ada53630198462020e09069a7ede37
Chris P <disenchanted@live.ca>
2020-03-02 23:44:26 -0800

    Setting up Firebase for iOS

--- iOSApp/iOSApp/ViewController.swift
+++ iOSApp/iOSApp/ViewController.swift
@@ -1,125 +1,58 @@
-//
-//  ViewController.swift
-//  iOSApp
-//
-//  Created by Javier Arroyo on 11/12/2019.
-//  Copyright © 2019 Javier Arroyo. All rights reserved.
-//
-
 import UIKit
 import SharedCode
+import Firebase
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    // View
-    @IBOutlet weak var mButton: UIButton!
-    @IBOutlet weak var mCounterLabel: UILabel!
-    @IBOutlet weak var mTableView: UITableView!
+struct Article{
+    let name: String!
+    let text: String!
+}
+
+class TableViewController: UITableViewController {
     
-    private var mCounterViewModel: CounterViewModel!
-    private var mGitHubViewModel: GitHubViewModel!
+    var list = [Article]()
     
-    // Table View Data
-    internal var mGitHubRepoList: [GitHubRepo] = []
+    @IBOutlet var mTableView: UITableView!
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        configView()
-        initViewModel()
-    }
-    
-    func configView() {
-        mButton.addTarget(self, action: #selector(didButtonClick), for: .touchUpInside)
         
-        mTableView.dataSource = self
-        mTableView.delegate = self
     }
     
-    func initViewModel() {
-        mCounterViewModel = CounterViewModel()
-        mGitHubViewModel = GitHubViewModel()
-        observeCounterViewModel()
-        observeGitHubViewModel()
-    }
-    
-    /****************************************************************************
-     * OBSERVER VIEW MODEL
-     ***************************************************************************/
-    func observeCounterViewModel() {
-        mCounterViewModel.mGetCounterLiveData.addObserver { (mCurrentState) in
-            if (mCurrentState is SuccessGetCounterState) {
-                let successState = mCurrentState as! SuccessGetCounterState
-                let response = (successState.response as! Response.Success)
-                let value = response.data as! Int
-                self.mCounterLabel.text = String(value)
+    override func viewDidAppear(_ animated: Bool) {
+        let db = Firestore.firestore()
+        
+        view.backgroundColor = #colorLiteral(red: 0.1802030457, green: 0.1802030457, blue: 0.1802030457, alpha: 1)
+        self.navigationItem.title = "Home"
+        
+        db.collection("conferences").document("DEFCON27").collection("articles").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                let articles = querySnapshot!.documents.map { Article(name: $0.data()["name"] as! String, text: $0.data()["text"] as! String)}
                 
-            } else if (mCurrentState is LoadingGetCounterState) {
-                self.mCounterLabel.text = "Loading"
-            } else if (mCurrentState is ErrorGetCounterState) {
-                self.mCounterLabel.text = "ERROR"
+                self.list = articles
+                self.tableView?.reloadData()
             }
-        
         }
     }
     
-    func observeGitHubViewModel() {
-        mGitHubViewModel.mGetGitHubRepoListLiveData.addObserver { (mCurrentState) in
-            if (mCurrentState is SuccessGetGitHubRepoListState) {
-                let successState = mCurrentState as! SuccessGetGitHubRepoListState
-                let response = (successState.response as! Response.Success)
-                let value = response.data as! [GitHubRepo]
-                self.onSuccessGetGitHubRepoList(list: value)
-                
-            } else if (mCurrentState is LoadingGetGitHubRepoListState) {
-                self.mCounterLabel.text = "Loading"
-            } else if (mCurrentState is ErrorGetGitHubRepoListState) {
-                self.mCounterLabel.text = "ERROR"
-            }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count + 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            return Bundle.main.loadNibNamed("SkullHeaderTableViewCell", owner: self, options: nil)?.first as! SkullHeaderTableViewCell
+        }
+        
+        let cell = Bundle.main.loadNibNamed("ArticleTableViewCell", owner: self, options: nil)?.first as! ArticleTableViewCell
             
-        }
-    }
-    
-    func onSuccessGetGitHubRepoList(list: [GitHubRepo]) {
-        update(list: list)
-    }
-    
-    /****************************************************************************
-     * ON CLICKS
-     ****************************************************************************/
-    @objc func didButtonClick(_ sender: UIButton) {
-        mCounterViewModel.getCounter()
-        mGitHubViewModel.getGitHubRepoList(username: "jarroyoesp")
-    }
-    
-    /*****************************************************************************
-     TABLE VIEW
-     ****************************************************************************/
-    internal func update(list: [GitHubRepo]) {
-        mGitHubRepoList.removeAll()
-        mGitHubRepoList.append(contentsOf: list)
-        mTableView.reloadData()
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mGitHubRepoList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "locationListCell", for: indexPath)
-        let entry = mGitHubRepoList[indexPath.row]
+        cell.mName.text = list[indexPath.row - 1].name
+        cell.mText.text = list[indexPath.row - 1].text
         
-        cell.textLabel?.text = entry.name
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let entryNum = mGitHubRepoList[indexPath.row].name
-    }
-    
-    
-    deinit {
-        mCounterViewModel.onCleared()
-    }
-    
 }
 
 
